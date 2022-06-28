73626160c66bd596ab56013c6be73bdbec9ac2b0
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-06-27 23:49:14 -0300

    Agragdo de funcion antiguedad

--- TP6/Empleados.cs
+++ TP6/Empleados.cs
@@ -8,10 +8,31 @@ public class Empleado{
     double sueldoBase;
     Cargo cargo;
 
-    public Empleado(string nombre, string apellido, DateTime fechaNacimiento, DateTime fechaIngresoEmpresa){
+    public Empleado(string nombre, string apellido, DateTime fechaNacimiento, DateTime fechaIngresoEmpresa, char estadoCivil, char genero, double sueldoBase){
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaNacimiento = fechaNacimiento;
         this.fechaIngresoEmpresa = fechaIngresoEmpresa;
+        this.estadoCivil = estadoCivil;
+        this.genero = genero;
+        this.sueldoBase = sueldoBase;
+        //this.cargo = cargo;
+    }
+
+    public void antiguedad(){
+        DateTime fechaHoy = new DateTime();
+        fechaHoy = DateTime.Now;
+        int edad, jubilacion;
+        TimeSpan Antiguedad = fechaHoy - this.fechaIngresoEmpresa;
+        edad = fechaHoy.Year - this.fechaNacimiento.Year;
+        if(this.genero == 'm'){
+            jubilacion = 65 - Convert.ToInt32(edad);
+        }else{
+            jubilacion = 60 - Convert.ToInt32(edad);
+        }    
+        Console.WriteLine($"El empleado {this.nombre} {this.apellido} lleva: {Antiguedad.Days} Dias en la empresa");
+        Console.WriteLine($"EDAD: {edad}");
+        Console.WriteLine($"Años restante para jubilacion: {jubilacion} años");
+
     }
 }
--- TP6/Program.cs
+++ TP6/Program.cs
@@ -1,15 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information;
 DateTime fechaNac1 = new DateTime(1998, 10, 16);
-DateTime fechaIngreso1 = DateTime.Today;
+DateTime fechaIngreso1 = new DateTime(2022, 05, 24);
 DateTime fechaNac2 = new DateTime(2000, 10, 16);
-DateTime fechaIngreso2 = DateTime.Today;
+DateTime fechaIngreso2 = new DateTime(2022, 05, 24);
 DateTime fechaNac3 = new DateTime(2002, 7, 29);
-DateTime fechaIngreso3 = DateTime.Today;
+DateTime fechaIngreso3 = new DateTime(2022, 05, 24);
 DateTime fechaNac4 = new DateTime(1996, 7, 30);
-DateTime fechaIngreso4 = DateTime.Today;
+DateTime fechaIngreso4 = new DateTime(2022, 05, 24);
 
-Empleado empleado1 = new Empleado("Santiago","Castillo", fechaNac1, fechaIngreso1);
-Empleado empleado2 = new Empleado("Agustin","Lobo", fechaNac2, fechaIngreso2);
-Empleado empleado3 = new Empleado("Nestor","Campos", fechaNac3, fechaIngreso3);
-Empleado empleado4 = new Empleado("Emanuel","Bilkis", fechaNac4, fechaIngreso4);
+Empleado empleado1 = new Empleado("Santiago","Castillo", fechaNac1, fechaIngreso1, 's', 'm', 60000);
+Empleado empleado2 = new Empleado("Agustin","Lobo", fechaNac2, fechaIngreso2, 's', 'm', 60000);
+Empleado empleado3 = new Empleado("Nestor","Campos", fechaNac3, fechaIngreso3,  's', 'm', 60000);
+Empleado empleado4 = new Empleado("Emanuel","Bilkis", fechaNac4, fechaIngreso4,  'c', 'm', 60000);
+
+empleado1.antiguedad();
 
